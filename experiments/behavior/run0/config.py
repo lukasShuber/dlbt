387ca5f407dee8c2d4d9aa42eb277b6f06962cf9
lukasShuber <lukas.hubus@gmail.com>
2026-04-21 20:35:57 +0200

    .

--- experiments/behavior/run0/config.py
+++ experiments/behavior/run0/config.py
@@ -49,7 +49,7 @@ PATIENCE_PHASE2 = 100
 LR              = 1e-2
 LR_ATTNPOOL     = 1e-5
 N_MC            = 100
-FREEZE_ENCODER  = False
+FREEZE_ENCODER  = True
 MAPPER_HIDDEN   = None
 
 RUN_TAG = "frozen" if FREEZE_ENCODER else "attnpool"
