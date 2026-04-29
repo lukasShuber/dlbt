b406b882a651f1a31d43da174f6703f6ac023619
lukasShuber <lukas.hubus@gmail.com>
2026-04-29 17:44:16 +0200

    .

--- experiments/simulations/04_lbt_recovery/config.py
+++ experiments/simulations/04_lbt_recovery/config.py
@@ -33,7 +33,7 @@ SEED          = 42
 N_TRIALS      = 100       # synthetic trials per (image, task) cell
 N_MC          = 10000      # MC samples for LbtAgent inference
 N_EPOCHS      = 200
-LR            = 0.1
+LR            = 0.3
 GRAD_CLIP     = 1.0
 
 # ---------------------------------------------------------------------------
