d6227b052a8ba1d61a222aad6c0048d640a05165
lukasShuber <lukas.hubus@gmail.com>
2026-04-30 16:43:36 +0200

    .

--- experiments/behavior/run1/03_lbt/config.py
+++ experiments/behavior/run1/03_lbt/config.py
@@ -59,7 +59,7 @@ NORMALIZED_UTILITY = True
 # Training
 # ---------------------------------------------------------------------------
 N_EPOCHS  = 500
-LR        = 0.005
+LR        = 0.01
 GRAD_CLIP = 1.0
 N_MC      = 50000       # MC samples for LbtAgent inference
 
