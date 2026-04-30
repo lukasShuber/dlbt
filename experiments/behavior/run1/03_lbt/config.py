49450db082054c1d371cdef295df89c47379f7ed
lukasShuber <lukas.hubus@gmail.com>
2026-04-30 16:06:48 +0200

    .

--- experiments/behavior/run1/03_lbt/config.py
+++ experiments/behavior/run1/03_lbt/config.py
@@ -77,10 +77,10 @@ INIT_SEED       = 0
 # ---------------------------------------------------------------------------
 # Task split
 # ---------------------------------------------------------------------------
-SPLIT_MODE    = "arity"       # "all" | "arity" | "random"
+SPLIT_MODE    = "random"       # "all" | "arity" | "random"
 
 # Used when SPLIT_MODE == "arity":
-TRAIN_ARITIES = [1]         # arities included in training
+TRAIN_ARITIES = [4]         # arities included in training
 HOLD_OUT_REST = True        # True → remaining arities go to val; False → no val
 
 # Used when SPLIT_MODE == "random":
