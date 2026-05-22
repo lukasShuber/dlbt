8c019ce60dbd3751b587c1475c00ed7d883547f2
lukasShuber <lukas.hubus@gmail.com>
2026-05-22 18:30:04 +0200

    .

--- dlbt/training/__init__.py
+++ dlbt/training/__init__.py
@@ -1,3 +1,3 @@
 from dlbt.training.metrics import multinomial_nll, mse, corrected_mse
 from dlbt.training.train_dlbt import train_dlbt, evaluate, TrainResult
-from dlbt.training.train_slda import fit_slda
+from dlbt.training.train_slda import fit_slda_logreg, slda_probe_matrix
