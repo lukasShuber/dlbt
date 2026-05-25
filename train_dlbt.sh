2f02218971c84cd8b022c8c8f4038217c223372e
lukasShuber <lukas.hubus@gmail.com>
2026-05-25 10:52:14 +0200

    .

--- train_dlbt.sh
+++ train_dlbt.sh
@@ -3,7 +3,7 @@
 #### SLURM JOB OPTIONS ##############################################
 #SBATCH --mail-user=lukas.s.huber@unibe.ch
 #SBATCH --mail-type=FAIL,END
-#SBATCH --job-name=ablations_attpool_5_seeds_no_median_correction
+#SBATCH --job-name=attpool_5_seeds_slda_dlbt
 #SBATCH --output=logs/output_%j.txt
 #SBATCH --error=logs/error_%j.txt
 
