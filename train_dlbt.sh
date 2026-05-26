16e19f5facf89e72ccfa9cbced8e83690f55882b
lukasShuber <lukas.hubus@gmail.com>
2026-05-26 17:55:15 +0200

    .

--- train_dlbt.sh
+++ train_dlbt.sh
@@ -3,7 +3,7 @@
 #### SLURM JOB OPTIONS ##############################################
 #SBATCH --mail-user=lukas.s.huber@unibe.ch
 #SBATCH --mail-type=FAIL,END
-#SBATCH --job-name=task_gen
+#SBATCH --job-name=task_gen_0.001
 #SBATCH --output=logs/output_%j.txt
 #SBATCH --error=logs/error_%j.txt
 
